5582b4f133528002a0707e9c73797a890d35e191
Rikard Lindström <ornotermes@gmail.com>
2022-04-17 19:43:36 +0200

    Basic mDNS support

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,18 +1,27 @@
 #include <WiFiManager.h>
+#include <WiFiUdp.h>
+#include <MDNS_Generic.h>
+
+#define DEBUG_PORT Serial
 
 //set up some strings depending on the build target
 #if defined(ESP32)
     String id = String((uint32_t)ESP.getEfuseMac(), HEX);
     String hostname = "pig-eon_" + id;
 #else
-    String serial = "0";
+    String id = "0";
     String hostname = "pig-eon";
 #endif
 
+String mdns_description = "Pig-eon Amateur radio modem (" + id + ")";
+
 WiFiManager wifiMgr; //WiFiManager manages connections to wireless networks
 
+WiFiUDP udp; //Construct UDP
+MDNS mdns(udp); //Construct mDNS
+
 void setup() {
-    Serial.begin(115200); //Set up serial debug console
+    DEBUG_PORT.begin(115200); //Set up serial debug console
 
     WiFi.mode(WIFI_STA); //Set WiFi to STA/Client mode
 
@@ -22,13 +31,21 @@ void setup() {
 
     bool res;
     if( wifiMgr.autoConnect(hostname.c_str()) ){ //use hostname as AP name if unable to connect
-        Serial.println("WiFiManager connected to WiFi");
+        DEBUG_PORT.println("WiFiManager connected to WiFi");
     }
     else {
-        Serial.println("WiFiManager could not connect to WiFi, config portal running");
+        DEBUG_PORT.println("WiFiManager could not connect to WiFi, config portal running");
     }
+
+    DEBUG_PORT.println("Registering mDNS hostname: " + hostname);
+    mdns.begin(WiFi.localIP(), hostname.c_str()); //Initiate mDNS and make it reachable with a .local hostname
+    
+    //Add mDNS service records to make services discoverable
+    mdns.addServiceRecord( String( mdns_description + " Web Server._http" ).c_str(), 80, MDNSServiceTCP);
 }
 
 void loop() {
-    wifiMgr.process();
+    wifiMgr.process(); //WiFiManager task
+
+    mdns.run(); //mDNS task
 }
