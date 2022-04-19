a10cc2bbb6d1fade028497dec0fe1fea3c25b1e8
Rikard Lindström <ornotermes@gmail.com>
2022-04-19 03:55:48 +0200

    Add an mDNS TXT records for sink/source

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -13,7 +13,40 @@
     String hostname = "pig-eon";
 #endif
 
+String repo_url = "https://github.com/ornotermes/pig-eon";
+
 String mdns_description = "Pig-eon Amateur radio modem (" + id + ")";
+
+const char* get_mdns_txt_str(String device) {
+    String records[] = {
+        "server-version=pulseaudio 15.0",
+        "user-name=root",
+        "machine-id=00000000000000000000000000000000",
+        "uname=Unknown",
+        "fqdn=" + hostname + ".local",
+        "cookie=0x00000000",
+        "device=" + device,
+        "rate=48000",
+        "channels=2",
+        "format=s16le",
+        "channel_map=front-left,front-right",
+        "subtype=hardware",
+        "description=WiFi Amateur radio modem",
+        "vendor-name=" + repo_url,
+        "product-name=Pig-eon (" + id + ")",
+        "class=sound",
+        "form-factor=internal",
+        "icon-name=computer-desktop"    
+    };
+    int records_count = sizeof(records)/sizeof(records[0]); //items in array = size of array in bytes / size of any array item in bytes
+    //DEBUG_PORT.println("mDNS txt array count: " + String( records_count ) );
+    String str = ""; //formated txt-record string
+    for( int i = 0; i < records_count; i++ ){
+        str += char( strlen( records[i].c_str() ) ); //length of parameter in bytes as raw char number
+        str += records[i]; //parameter text
+    }
+    return (str.c_str());
+}
 
 WiFiManager wifiMgr; //WiFiManager manages connections to wireless networks
 
@@ -53,9 +86,12 @@ void setup() {
     mdns.addServiceRecord( String( mdns_description + " App._http" ).c_str(), 80, MDNSServiceTCP);//Publish webserver for management
     DEBUG_PORT.println("✅ App Web Server");
 
-    mdns.addServiceRecord( String( mdns_description + " PulseAudio Sink._pulse-sink" ).c_str(), 4713, MDNSServiceTCP); //Publish sink for PulseAudio module-zeroconf-discover
+    const char *str = get_mdns_txt_str("output"); //Get a txt record to send audio server properties with mDNS
+    //DEBUG_PORT.println(str);
+    mdns.addServiceRecord( String( mdns_description + " PulseAudio Sink._pulse-sink" ).c_str(), 4713, MDNSServiceTCP, str); //Publish sink/output for PulseAudio module-zeroconf-discover
     DEBUG_PORT.println("✅ PulseAudio Sink");
-    mdns.addServiceRecord( String( mdns_description + " PulseAudio Source._pulse-source" ).c_str(), 4713, MDNSServiceTCP); //Publish source for PulseAudio module-zeroconf-discover
+    str = get_mdns_txt_str("input");
+    mdns.addServiceRecord( String( mdns_description + " PulseAudio Source._pulse-source" ).c_str(), 4713, MDNSServiceTCP, str); //Publish source/input for PulseAudio module-zeroconf-discover
     DEBUG_PORT.println("✅ PulseAudio Source");
 }
 
